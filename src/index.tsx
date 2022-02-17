4a798772272c6008184dd20c7d9eb0dea95b88ba
River <swimmingr@gmail.com>
2022-02-17 15:49:04 +0900

    second commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { RecoilRoot } from "recoil";
 import './index.css';
 import App from './App';
 import { ThemeProvider } from "styled-components";
-import reportWebVitals from './reportWebVitals';
+
 import { darkTheme } from './theme';
 import {createGlobalStyle} from "styled-components";
 const GlobalStyle = createGlobalStyle`
@@ -87,4 +87,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
